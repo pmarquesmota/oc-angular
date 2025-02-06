4db5a7b4878999c4203afca9f3fc91f682edf70b
Paul Marques Mota <paul@sas-kalliste.fr>
2025-02-06 14:06:17 +0100

    complete

---
+++ java-ci.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import subprocess
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def build():
+	subprocess.run(["npm", "run", "build"])
+
+@app.command()
+def test():
+	subprocess.run(["npm", "test"])
+
+@app.command()
+def pack():
+	subprocess.run(["npm", "pack"])
+
+@app.command()
+def publish():
+	subprocess.run(["npm", "publish"])
+
+if __name__ == "__main__":
+    app()
+
